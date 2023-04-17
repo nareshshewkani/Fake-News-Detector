9c63a736d0d6a30ca99162b428b397cf5920e497
sahil parab <sahiparab6117@gmail.com>
2023-04-18 00:02:11 +0530

    final commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Create flask app
+flask_app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("tfidf_vectorizer.pkl","rb"))
+@flask_app.route("/")
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict", methods = ["POST"])
+# def predict():
+#     float_features = [float(x) for x in request.form.values()]
+#     features = [np.array(float_features)]
+#     prediction = model.predict(features)
+#     return render_template("index.html", prediction_text = "The News is {} %".format(prediction))
+
+
+def predict():
+    float_features = [x for x in request.form.values()]
+    float_features = [float_features[1]]
+    features = np.array(float_features)
+    print(features.shape)
+    #Transform the input
+    # Initialize a TfidfVectorizer
+    # tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+
+    features = vectorizer.transform(features)
+
+    prediction = model.predict(features)
+    return render_template("index.html", prediction_text = "This news is {} ".format(prediction[0]))
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)
+
